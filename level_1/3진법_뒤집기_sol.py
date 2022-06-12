861cd3b7e848ce5a63e24e59743f5b7e9cb16acc
gialeee <63530976+gialeee@users.noreply.github.com>
2022-06-12 20:05:22 +0900

    몰랐던 내용 주석 추가

--- level_1/3진법_뒤집기_sol.py
+++ level_1/3진법_뒤집기_sol.py
@@ -4,5 +4,7 @@ def solution(n):
         tmp += str(n % 3)
         n = n // 3
 
-    answer = int(tmp, 3)  # int()함수를 이용해서 3진법으로 나타낸 숫자 반환
+    answer = int(tmp, 3)  # int(value, base) 함수를 이용해서 3진법으로 나타낸 숫자 반환
+                          # value : string 또는 number을 int형으로 변환
+                          # base : 넘버 포맷(n진법)을 결정 / default : 10
     return answer
